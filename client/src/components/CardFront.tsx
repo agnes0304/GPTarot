769b6c1acdd48c2c18dcf564faf5d7b16af2441b
agnes0304 <jiwoochoi0304@gmail.com>
2023-09-25 17:24:41 +0900

    added tilt effect on Front side

--- client/src/components/CardFront.tsx
+++ client/src/components/CardFront.tsx
@@ -1,9 +1,34 @@
-import { FC } from "react";
+import { FC, useRef, useEffect } from "react";
 import classes from "./CardFront.module.css";
+import VanillaTilt from "vanilla-tilt";
+
+interface HTMLDivElementWithTilt extends HTMLDivElement {
+  vanillaTilt: {
+    destroy: () => void;
+  };
+}
 
 const CardFront: FC = () => {
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (cardRef.current) {
+      VanillaTilt.init(cardRef.current, {
+        max: 25,
+        speed: 400,
+        glare: true,
+        "max-glare": 0.5,
+      });
+    }
+
+    const current = cardRef.current as HTMLDivElementWithTilt;
+
+    return () => {
+      current.vanillaTilt.destroy();
+    };
+  }, []);
   return (
-    <div className={classes.cardFront}>
+    <div className={classes.cardFront} ref={cardRef}>
       <p>Front</p>
     </div>
   );
