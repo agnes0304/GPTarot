f0e274acd29623c71d3b4ca6bb4fd8daf4ae1e3a
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-02 17:29:49 +0900

    adding vercel json

--- client/src/axios/axiosInstance.tsx
+++ client/src/axios/axiosInstance.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://gptarot.fly.dev",
   // baseURL: "http://localhost:8080",
+  timeout: 5000,
 });
 
 export default axiosInstance;
