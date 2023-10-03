7f8bcca98e838638cce6f83a834ffd943d400827
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-03 17:35:31 +0900

    hover test

--- src/components/MobileSpread.tsx
+++ src/components/MobileSpread.tsx
@@ -7,30 +7,30 @@ interface MobileSpreadProps {
 }
 
 const MobileSpread: FC<MobileSpreadProps> = ({ handleClick }) => {
-  const [hoverCard, sethoverCard] = useState<number | null>(null);
+  const [hoverCard, setHoverCard] = useState<boolean>(false);
   const cards = Array(5).fill(null);
 
-  const handleHoverStart = (index: number) => {
-    sethoverCard(index);
+  const handleHoverStart = () => {
+    setHoverCard(true);
   };
   const handleHoverEnd = () => {
-    sethoverCard(null);
+    setHoverCard(false);
   };
 
   return (
-    <div className="flex justify-center items-center overflow-x-auto w-[92%] pt-[40px] ml-[10px]">
+    <div 
+      className={`flex justify-center items-center overflow-x-auto w-[92%] pt-[40px] ml-[10px] transform transition-all ease-in-out duration-300 ${hoverCard ? "-translate-y-5" : ""}`}
+      onMouseEnter={handleHoverStart}
+      onMouseLeave={handleHoverEnd}
+      onTouchStart={handleHoverStart}
+      onTouchEnd={handleHoverEnd}
+    >
       {cards.map((_, index) => (
         <div
           key={index}
           id={`card${index + 1}`}
           onClick={(e) => handleClick(e)}
-          onTouchStart={() => handleHoverStart(index)}
-          onTouchEnd={handleHoverEnd}
-          className={`${
-            classes.card
-          } w-[200px] h-[356px] cursor-pointer flex items-center justify-center transform transition-all ease-in-out duration-300 ${
-            hoverCard === index ? "-translate-y-5" : ""
-          } relative`}
+          className={`${classes.card} w-[200px] h-[356px] cursor-pointer flex items-center justify-center relative`}
           style={{
             marginLeft: index !== 0 ? "-160px" : "0",
             zIndex: cards.length - index,
