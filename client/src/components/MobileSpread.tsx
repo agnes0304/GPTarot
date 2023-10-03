afc1c260dc314de95ad7ccfb00f34f4869504e2c
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-03 17:01:56 +0900

    mobile ongoing

---
+++ client/src/components/MobileSpread.tsx
@@ -0,0 +1,50 @@
+import { FC, useState } from "react";
+import classes from "../styles/MobileSpread.module.css";
+import back from "../assets/cardBack.webp";
+
+interface MobileSpreadProps {
+  handleClick: (e: unknown) => void;
+}
+
+const MobileSpread: FC<MobileSpreadProps> = ({ handleClick }) => {
+  const [hoverCard, sethoverCard] = useState<number | null>(null);
+  const cards = Array(5).fill(null);
+
+  const handleHoverStart = (index: number) => {
+    sethoverCard(index);
+  };
+  const handleHoverEnd = () => {
+    sethoverCard(null);
+  };
+
+  return (
+    <div className="flex justify-center items-center overflow-x-auto w-[600px] pt-[40px]">
+      {cards.map((_, index) => (
+        <div
+          key={index}
+          id={`card${index + 1}`}
+          onClick={(e) => handleClick(e)}
+          onTouchStart={() => handleHoverStart(index)}
+          onTouchEnd={handleHoverEnd}
+          className={`${
+            classes.card
+          } w-[200px] h-[356px] cursor-pointer flex items-center justify-center transform transition-all ease-in-out duration-300 ${
+            hoverCard === index ? "-translate-y-5" : ""
+          } relative`}
+          style={{
+            marginLeft: index !== 0 ? "-140px" : "0",
+            zIndex: cards.length - index,
+          }}
+        >
+          <img
+            src={back}
+            alt="back side of card"
+            className="object-cover rounded-lg"
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MobileSpread;
