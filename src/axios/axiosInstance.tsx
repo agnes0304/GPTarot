9a2d7055ad7970e032229443883f57b5cdc13a7a
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-16 11:14:47 +0900

    timeout:5000 to 10000

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://gptarot.fly.dev",
   // baseURL: "http://localhost:8080",
-  timeout: 5000,
+  timeout: 10000,
 });
 
 export default axiosInstance;
