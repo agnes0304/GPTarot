1214ff1ac92f26192c05763b3d10a23fa23408d7
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-16 12:37:11 +0900

    error page

--- src/axios/axiosInstance.tsx
+++ src/axios/axiosInstance.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://gptarot.fly.dev",
   // baseURL: "http://localhost:8080",
-  timeout: 10000,
+  timeout: 12000,
 });
 
 export default axiosInstance;
