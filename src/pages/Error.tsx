ecd6ea9e4593e75be8ce6dddb7a6497fdd8db388
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-16 11:32:37 +0900

    error page test

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,9 +1,14 @@
 import { FC } from "react";
+import error404 from "../assets/error404.webp";
+// import error500 from "../assets/error500.webp";
 
 const Error: FC = () => {
-    return (<div>
-        <h1>OOPS</h1>
-    </div>);
-}
+  return (
+    <div>
+      <h1>OOPS!</h1>
+        <img src={error404} alt="404" />
+    </div>
+  );
+};
 
 export default Error;
