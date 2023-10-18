d4850fe73cd04c2fdccc0fbb14d53538dfed2080
agnes0304 <jiwoochoi0304@gmail.com>
2023-10-18 18:01:31 +0900

    modified loading component: ko

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,26 +1,46 @@
-import { FC } from "react";
+import { FC, useState, useEffect } from "react";
 import classes from "../styles/Loading.module.css";
 import { useLanguage } from "../hooks/useLanguage";
 import { InnerText } from "../data/InnerText";
 
 const Loading: FC = () => {
   const { language } = useLanguage();
+  const [showSecond, setShowSecond] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSecond(true);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-      <div className="flex justify-center items-center flex-col">
+    <div className="flex justify-center items-center flex-col gap-1">
+      {showSecond ? (
+        <>
+          <h1 className={`${classes.text} text-violet-400`}>
+            {language === "ko" && InnerText.translate.first}
+          </h1>
+          <p className={`${classes.text} text-violet-400 text-sm`}>
+            {language === "ko" && InnerText.translate.second}
+          </p>
+        </>
+      ) : (
         <h1 className={`${classes.text} text-violet-400`}>
           {language === "ko" ? InnerText.loading.ko : InnerText.loading.en}
         </h1>
-        <div className={`${classes.load} mt-4`}>
-          <div className="relative w-[52px] h-[52px] border-2 border-violet-400 rounded-[100%] mx-auto my-0">
-            <div
-              className={`${classes.ballHolder} absolute flex justify-center items-center w-[12px] h-[45px] left-[18px] top-0`}
-            >
-              <div className="absolute top-[-10px] left-0 w-[15px] h-[15px] rounded-[100%] bg-violet-400"></div>
-            </div>
+      )}
+      <div className={`${classes.load} mt-4`}>
+        <div className="relative w-[52px] h-[52px] border-2 border-violet-400 rounded-[100%] mx-auto my-0">
+          <div
+            className={`${classes.ballHolder} absolute flex justify-center items-center w-[12px] h-[45px] left-[18px] top-0`}
+          >
+            <div className="absolute top-[-10px] left-0 w-[15px] h-[15px] rounded-[100%] bg-violet-400"></div>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
--- src/data/InnerText.tsx
+++ src/data/InnerText.tsx
@@ -2,6 +2,7 @@ export const InnerText = {
     userInput : { ko:"무엇이 궁금하신가요?", en:"Please enter your question" },
     sampleQ : {ko:"아래는 예시 질문입니다. 클릭하면 바로 카드 선택으로!", en: { first: "The following are sample questions.", second: "Click to select a card right away!" }},
     loading: {ko:"답변을 가지고 오고 있어요...", en:"Loading..."},
+    translate: {first:"답변을 한글로 해석 중이에요!", second: "잠시만 기다려 주세요😊"},
     cardDeck: {ko:"아래 카드 중 하나를 선택해주세요", en:"Please select one of the cards below"},
     result: {ko:"카드를 클릭하면 답을 확인할 수 있어요", en:"Click the card to see the answer"},
     resetBtn: {ko:"질문하러 가기", en:"Go to ask"},
