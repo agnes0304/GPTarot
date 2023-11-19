acb56f771d034de218e3d4a9b7a174aa72a53d59
agnes0304 <jiwoochoi0304@gmail.com>
2023-11-19 17:38:24 +0900

    added kako logo

--- src/components/SharerBtn.tsx
+++ src/components/SharerBtn.tsx
@@ -5,6 +5,7 @@ import { FC } from "react";
 import { useParams } from "react-router-dom";
 import { useApiResponse } from "../hooks/useApiResponse";
 import axiosInstance from "../axios/axiosInstance";
+import kakaoLogo from "../assets/kakao.png";
 
 // db에 post하는 요청보내고 url 생성해줘야 함.
 const SharerBtn: FC = () => {
@@ -66,7 +67,44 @@ const SharerBtn: FC = () => {
   };
 
   // TODO: 카카오톡 공유하기
-  // const kakaoShare = () => {};
+  // const kakaoShare = () => {
+  //   const url = `https://gptarot.jiwoo.best/answer/${nanoId}`;
+
+  //   Kakao.Link.createDefaultButton({
+  //     container: "#btnKakao", // 카카오공유버튼ID
+  //     objectType: "feed",
+  //     content: {
+  //       title: "GPTarot | 지피타로",
+  //       description: "지피티가 읽어주는 타로점",
+  //       imageUrl: `https://gptarot.jiwoo.best/${bodyData.cardId}.webp`,
+  //       link: {
+  //         mobileWebUrl: url,
+  //         webUrl: url,
+  //       },
+  //     },
+  //   });
+  // };
+
+  // function shareKakao() {
+
+  //   // 사용할 앱의 JavaScript 키 설정
+  //   Kakao.init('카카오에서_발급받은_API키');
+
+  //   // 카카오링크 버튼 생성
+  //   Kakao.Link.createDefaultButton({
+  //     container: '#btnKakao', // 카카오공유버튼ID
+  //     objectType: 'feed',
+  //     content: {
+  //       title: "개발새발", // 보여질 제목
+  //       description: "개발새발 블로그입니다", // 보여질 설명
+  //       imageUrl: "devpad.tistory.com/", // 콘텐츠 URL
+  //       link: {
+  //          mobileWebUrl: "devpad.tistory.com/",
+  //          webUrl: "devpad.tistory.com/"
+  //       }
+  //     }
+  //   });
+  // }
 
   const handleClick = async () => {
     try {
@@ -94,6 +132,13 @@ const SharerBtn: FC = () => {
         onClick={twitterShare}
       >
         <FontAwesomeIcon icon={faTwitter} />
+      </button>
+      <button
+        className="flex justify-center items-center w-[40px] h-[40px] p-3 rounded-full cursor-pointer text-violet-400/50 hover:text-violet-400 hover:border-violet-400 hover:bg-violet-500/50"
+        type="button"
+        // onClick={kakaoShare}
+      >
+        <img src={kakaoLogo} alt="kakao" />
       </button>
     </>
   );
