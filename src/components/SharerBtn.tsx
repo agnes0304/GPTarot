2642c644c87a22ab1df52a0624259abf94675f5a
agnes0304 <jiwoochoi0304@gmail.com>
2023-11-19 16:08:27 +0900

    added twitter sharer

--- src/components/SharerBtn.tsx
+++ src/components/SharerBtn.tsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUpFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 import { FC } from "react";
 import { useParams } from "react-router-dom";
 import { useApiResponse } from "../hooks/useApiResponse";
@@ -17,7 +18,7 @@ const SharerBtn: FC = () => {
     cardId: apiResponse.cardId,
   };
 
-  //  Clipboard API 사용 제한이 있어서, fallback 함수를 만들어줌. -> TODO
+  //  Clipboard API 사용 제한이 있어서, fallback 함수를 만들어줌.
   const copyToClipboardFallback = (url: string) => {
     const textarea = document.createElement("textarea");
     textarea.textContent = url;
@@ -42,29 +43,28 @@ const SharerBtn: FC = () => {
   // Clipboard API 사용
   const copyToClipboard = async (url: string) => {
     try {
-      // Check if the Clipboard API is supported
       if (navigator.clipboard) {
         await navigator.clipboard.writeText(url);
-        alert("The link has been copied.");
+        alert("링크가 복사되었습니다.");
       } else {
-        // Fallback if Clipboard API is not available
         copyToClipboardFallback(url);
       }
     } catch (err) {
-      console.error("Error copying to clipboard", err);
-      // Using fallback in case of an error with Clipboard API
+      console.error("링크 복사에 실패했습니다.", err);
       copyToClipboardFallback(url);
     }
   };
 
   // TODO: 트위터 공유하기
-  // const twitterShare = () => {
-  //   const url = `https://gptarot.jiwoo.best/answer/${nanoId}`;
-  //   const text = `GPT가 알려주는 오늘의 운세`;
-  //   // const hashtags = "GPTarot";
-  //   const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(text)}&url=${encodeURIComponent(url)}`;
-  //   window.open(twitterUrl);
-  // };
+  const twitterShare = () => {
+    const url = `https://gptarot.jiwoo.best/answer/${nanoId}`;
+    const text = `GPT가 알려주는 오늘의 운세`;
+    // const hashtags = "GPTarot";
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      text
+    )}&url=${encodeURIComponent(url)}`;
+    window.open(twitterUrl);
+  };
 
   // TODO: 카카오톡 공유하기
   // const kakaoShare = () => {};
@@ -73,21 +73,30 @@ const SharerBtn: FC = () => {
     try {
       const url = `https://gptarot.jiwoo.best/answer/${nanoId}`;
       copyToClipboard(url);
-
       await axiosInstance.post("/save", bodyData);
     } catch (error) {
+      alert("결과 저장에 실패했습니다.");
       console.error(error);
     }
   };
 
   return (
-    <button
-      className="flex justify-center items-center w-[40px] h-[40px] p-3 rounded-full cursor-pointer text-violet-400/50 hover:text-violet-400 hover:border-violet-400 hover:bg-violet-500/50"
-      type="button"
-      onClick={handleClick}
-    >
-      <FontAwesomeIcon icon={faArrowUpFromBracket} />
-    </button>
+    <>
+      <button
+        className="flex justify-center items-center w-[40px] h-[40px] p-3 rounded-full cursor-pointer text-violet-400/50 hover:text-violet-400 hover:border-violet-400 hover:bg-violet-500/50"
+        type="button"
+        onClick={handleClick}
+      >
+        <FontAwesomeIcon icon={faArrowUpFromBracket} />
+      </button>
+      <button
+        className="flex justify-center items-center w-[40px] h-[40px] p-3 rounded-full cursor-pointer text-violet-400/50 hover:text-violet-400 hover:border-violet-400 hover:bg-violet-500/50"
+        type="button"
+        onClick={twitterShare}
+      >
+        <FontAwesomeIcon icon={faTwitter} />
+      </button>
+    </>
   );
 };
 
